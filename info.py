796c0117a1c05c937408ee78dfce9da98ec3ff6d
developeranandtech <163002965+developeranandtech@users.noreply.github.com>
2024-03-19 22:09:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -52,8 +52,8 @@ BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 RemoveBG_API = environ.get("RemoveBG_API", "")
 
 # url shortner
-SHORT_URL = environ.get("SHORT_URL")
-SHORT_API = environ.get("SHORT_API")
+SHORT_URL = environ.get("SHORT_URL", "omegalinks.in")
+SHORT_API = environ.get("SHORT_API" ,"893a28f1b72863da2a2744f80e7805677af5d7c5")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
